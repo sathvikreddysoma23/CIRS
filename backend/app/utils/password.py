2f8511de70d9fc6b2bb95dfa606d02b96ffe293f
sathvikreddysoma23 <sathvikreddysoma@gmail.com>
2026-04-09 23:48:49 +0530

    Explicitly disable passlib bcrypt truncate error

--- backend/app/utils/password.py
+++ backend/app/utils/password.py
@@ -1,6 +1,10 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"], 
+    deprecated="auto",
+    bcrypt__truncate_error=False  # Tell passlib to truncate instead of crashing
+)
 
 
 def hash_password(plain_password: str) -> str:
